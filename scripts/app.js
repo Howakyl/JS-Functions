b8f1c5c1464e32dded2e6d8df6a4ce165f0cbec3
Howakyl96 <howakyl@gmail.com>
2020-09-13 21:38:36 -0700

    Commit 1 - Verbal questions

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+// VERBAL QUESTIONS 
+
+//1 Parameters are named variables put in a function to be used later. Arguments are parameters with actual values, used when the function is called.
+
+//2 Return allows you to "return" a value from a function, allowing the value to be used. Console.log only writes the values in text; it doesn't define anything.
+
+//3 Functions being able to return values gives us the ability to re-use functions to call them later in loops, functions, etc. Thus allowing us to have to write much short code, and not having to repeat ourselves.
