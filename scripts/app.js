a07e473408876732a788132da7c971f994323381
Howakyl96 <howakyl@gmail.com>
2020-09-13 22:15:59 -0700

    Commit 2 - Palindrome

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,20 @@
 //2 Return allows you to "return" a value from a function, allowing the value to be used. Console.log only writes the values in text; it doesn't define anything.
 
 //3 Functions being able to return values gives us the ability to re-use functions to call them later in loops, functions, etc. Thus allowing us to have to write much short code, and not having to repeat ourselves.
+
+//////////////////////////////////////////////////////////
+
+//PALINDROME AGAIN
+
+function checkPalindrome (string) {
+    const reversed = string.toUpperCase().split("").reverse().join("");
+    if (reversed === string.toUpperCase()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(checkPalindrome('Radar'));
+console.log(checkPalindrome("Borscht"));
+
+//////////////////////////////////////////////////////////
